2b214c724b781ddcbc39fc243a7f831f15112eab
Francis Stephens <francisstephens@gmail.com>
2015-06-11 12:50:46 +0100

    Adding Parse(string) and MustParse(string)

--- dec_util.go
+++ dec_util.go
@@ -4,10 +4,18 @@ import (
 	"errors"
 )
 
-func NewDecFromString(s string) (*Dec, error) {
+func Parse(s string) (*Dec, error) {
 	d, ok := NewDec(0, 0).SetString(s)
 	if !ok {
 		return nil, errors.New("Bad Decimal string '" + s + "'")
 	}
 	return d, nil
+}
+
+func MustParse(s string) *Dec {
+	d, ok := NewDec(0, 0).SetString(s)
+	if !ok {
+		panic("Bad Decimal string '" + s + "'")
+	}
+	return d
 }
